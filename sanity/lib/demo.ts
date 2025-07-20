f6753d51d3d3f2afdc96f9705b7c05a72a60dab5
b11056017 <b11056017@mail.npust.edu.tw>
2025-07-20 16:54:25 +0800

    文字更新

--- sanity/lib/demo.ts
+++ sanity/lib/demo.ts
@@ -6,54 +6,19 @@ export const title = "Blog.";
 
 export const description = [
   {
-    _key: "9f1a629887fd",
-    _type: "block",
+    _key: '9f1a629887fd',
+    _type: 'block',
     children: [
       {
-        _key: "4a58edd077880",
-        _type: "span",
+        _key: '4a58edd077880',
+        _type: 'span',
         marks: [],
-        text: "A statically generated blog example using ",
-      },
-      {
-        _key: "4a58edd077881",
-        _type: "span",
-        marks: ["ec5b66c9b1e0"],
-        text: "Next.js",
-      },
-      {
-        _key: "4a58edd077882",
-        _type: "span",
-        marks: [],
-        text: " and ",
-      },
-      {
-        _key: "4a58edd077883",
-        _type: "span",
-        marks: ["1f8991913ea8"],
-        text: "Sanity",
-      },
-      {
-        _key: "4a58edd077884",
-        _type: "span",
-        marks: [],
-        text: ".",
+        text: '使用 Next.js 框架，搭配 Sanity 作為內容管理系統，以靜態生成的方式建置',
       },
     ],
-    markDefs: [
-      {
-        _key: "ec5b66c9b1e0",
-        _type: "link",
-        href: "https://nextjs.org/",
-      },
-      {
-        _key: "1f8991913ea8",
-        _type: "link",
-        href: "https://sanity.io/",
-      },
-    ],
-    style: "normal",
+    markDefs: [],
+    style: 'normal',
   },
-];
+]
 
 export const ogImageTitle = "A Next.js Blog with a Native Authoring Experience";
